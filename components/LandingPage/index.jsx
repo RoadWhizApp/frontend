a0fd8d65409a00d85a75980c8a53e6873d6d4a26
Nam Choi <namchoi@WKMZT9A1B36A.global.publicisgroupe.net>
2023-03-26 13:41:54 -0400

    init push

--- components/LandingPage/index.jsx
+++ components/LandingPage/index.jsx
@@ -15,27 +15,28 @@ function LandingPage(props) {
     inputType,
     inputPlaceholder,
     login,
-    setClientInfo
+    setClientInfo,
   } = props;
 
   const [clientData, setClientData] = React.useState(null);
   const [clientId, setClientId] = React.useState(null);
 
-  const baseURL = "https://us-central1-burner-manchoud1.cloudfunctions.net/bbbhackathon";
+  const baseURL =
+    "https://us-central1-burner-manchoud1.cloudfunctions.net/bbbhackathon";
 
   const fetchClientData = () => {
     if (clientId) {
-      axios.post(baseURL, {
-        // Example "Customer_ID": "000006"
-        "Customer_ID": clientId
-      })
+      axios
+        .post(baseURL, {
+          // Example "Customer_ID": "000006"
+          Customer_ID: clientId,
+        })
         .then((response) => {
           setClientData(response.data);
           setClientInfo(response.data);
         });
     }
-
-  }
+  };
 
   const history = useHistory();
 
@@ -43,22 +44,44 @@ function LandingPage(props) {
     if (clientData) {
       history.push("/desktop-1");
     }
-  }, [clientData])
+  }, [clientData]);
 
   return (
     <div className="container-center-horizontal">
       <div className="landing-page screen">
-        <div className="overlap-group1-6" style={{ backgroundImage: `url(${overlapGroup1})` }}>
+        <div
+          className="overlap-group1-6"
+          style={{ backgroundImage: `url(${overlapGroup1})` }}
+        >
           <img className="rectangle-1-1" src={rectangle1} alt="Rectangle 1" />
           <h1 className="title-4">{title}</h1>
-          <div className="overlap-group-12" style={{ backgroundImage: `url(${overlapGroup})` }}>
-            <img className="logo-shapes-7-1" src={logoShapes7} alt="Logo Shapes 7" />
+          <div
+            className="overlap-group-12"
+            style={{ backgroundImage: `url(${overlapGroup})` }}
+          >
+            <img
+              className="logo-shapes-7-1"
+              src={logoShapes7}
+              alt="Logo Shapes 7"
+            />
           </div>
           <div className="rectangle-32"></div>
           <img className="mesh-08-1" src={mesh08} alt="Mesh-08" />
-          <div className="customer-id jost-bold-big-stone-24px">{customerId}</div>
-          <input className="customer-id-1" name="customerid" placeholder={inputPlaceholder} type={inputType} onChange={(e) => setClientId(e.target.value)} required />
-          <button className="login jost-bold-big-stone-24px" onClick={fetchClientData}>
+          <div className="customer-id jost-bold-big-stone-24px">
+            {customerId}
+          </div>
+          <input
+            className="customer-id-1"
+            name="customerid"
+            placeholder={inputPlaceholder}
+            type={inputType}
+            onChange={(e) => setClientId(e.target.value)}
+            required
+          />
+          <button
+            className="login jost-bold-big-stone-24px"
+            onClick={fetchClientData}
+          >
             {login}
           </button>
         </div>
